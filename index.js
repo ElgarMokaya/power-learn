06cc052fa6f8e2523424bb5b165b325bab263364
ElgarMokaya <elgarmokaya01@gmail.com>
2022-08-07 23:33:30 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+//
+//function toggleMobileMenu(this){
+   // menubar.classList.toggle('open');
+//}
+//
+
+function myFunction() {
+    var x = document.getElementById("mylinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
